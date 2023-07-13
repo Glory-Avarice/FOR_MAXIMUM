1bfb682c575d42606e63d2df4ce71f6185277ea1
Kirill <kirill16102006@icloud.com>
2023-07-13 12:26:16 +0500

    update for lesson5

--- django/advertisements/app_advertisements/views.py
+++ django/advertisements/app_advertisements/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse('Мы сделали это!')
+
 def index(request):
-    return HttpResponse('Мы сделали это!')
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
